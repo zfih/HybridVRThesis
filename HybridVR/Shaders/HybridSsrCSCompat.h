dac8eecda3993188a4d64e088d1256407462d657
LasseKB <LasseKB@hotmail.com>
2020-09-14 13:46:46 +0200

    Revert "Checkpoint"
    This reverts commit e1e181d4ac075e1198a344e0565361a896340f4e.

--- HybridVR/Shaders/HybridSsrCSCompat.h
+++ HybridVR/Shaders/HybridSsrCSCompat.h
@@ -7,33 +7,28 @@
 struct HybridSsrConstantBuffer
 {
 	float4 RenderTargetSize;
-
+	
 	float4x4 View;
 	float4x4 Projection;
 	float4x4 InvProjection;
-
+	
 	float NearPlaneZ;
 	float FarPlaneZ;
-
+	
 	// (Width, Height, 1 / Width, 1 / Height)
 
 	// Unclear
 	float SSRScale;
-
-	/**
-	 * Based on how far away from the camera the depth is,
-	 * adding a bit of extra thickness can help improve some
-	 * artifacts. Driving this value up too high can cause
-	 * artifacts of its own.
-	 */
+	
+	// Thickness to ascribe to each pixel in the depth buffer
 	float ZThickness;
 
 	// Step in horizontal or vertical pixels between samples.
 	//  Float because integer math is slow on GPUs.
 	float Stride;
-
+	
 	// Max number of iterations. More = Prettier but more expensive 
-	float MaxSteps;
+	float MaxSteps; 
 
 	// Maximum distance to trace before returning a miss
 	float MaxDistance;
