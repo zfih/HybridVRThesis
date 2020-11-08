a18daced1e053b884a39eb220407b763984f914a
Freemedude <daniel_g_hansen@hotmail.com>
2020-11-08 10:33:16 +0100

    Simplify IAPC reprojection angle logic

--- HybridVR/Shaders/ReprojectionPS.hlsl
+++ HybridVR/Shaders/ReprojectionPS.hlsl
@@ -114,7 +114,7 @@ MRT main(VertexOutput vOut)
 	float depth = gDepthTex.SampleLevel(gLinearSampler, vOut.texC, 0);
 	// todo(Danh) 13:35 07/08: Pass in IPD
 	float IPD = 0.065; // This is the IPD for the HTC Vive Pro Eye
-	float angle = abs(2 * atan(IPD / (2 * depth)) - PI);
+	float angle  = PI - 2 * atan(IPD / (2 * depth));
 
 	// Ratio is 0 when we're reusing reflections
 	// Ratio is > 0  but < 1 when we're blending reflections
